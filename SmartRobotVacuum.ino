0b31db80684e1898cf39324c75c35bb5df380b3e
iCAROS7 <hominlab@gmail.com>
2022-12-16 11:11:46 +0900

    M: loop() 코드 메서드 분리 및 수동 모드 충돌방지 메커니즘 추가

--- SmartRobotVacuum.ino
+++ SmartRobotVacuum.ino
@@ -213,36 +213,65 @@ void scanWay() {
   }
 }
 
+/* 통합 원격 조정 메서드 */
+void hybridRC() {
+  if (cmd == 'D' || btn == 90) { right(); }		// Turn right via BT 'D' or IR num_6
+  if (cmd == 'C' || btn == 8) { left(); }		// Turn left via BT 'C' or IR num_3
+  if (cmd == 'S' || btn == 28) { stop(); }		// Set pwmSpeed 0 via BT 'S' or IR num_5
+  if (cmd == 'A' || btn == 24) {// Chnage direction to forward via BT 'A' or IR num_2
+    if (!mov) {
+      forward(1);
+      mov = true;
+    }	// If last direction is reverse then set pwmSpeed 0 and change direction
+    else {
+      forward(0);
+      mov = true;
+    }
+    mov = true;			// Set last direction to forward
+  }
+  if (cmd == 'B' || btn == 82) {// Change direction to rear via BT 'B' or IR num_8
+    if (mov) { reverse(1); }	// If last direction is reverse then set pwmSpeed 0 and change direction
+    else { reverse(0); }
+    mov = false;		// Set last direction to reverse
+  }
+}
+
+/* 근처에 장애물 존재 유무 판단 메서드 */
+bool scanWall() {
+  if (dis[0] < 7 || dis[1] < 7 || dis[2] < 10) { return true; }
+
+  return false;
+}
+
 void loop() {
   btCmdIn();	// Call Bluetooth command
   irRx();		// Call IR command
   ultrasonic();		// Scanning Ultrasonic sensor data to global var
 
-  if (cmd == 'R' || btn == 66) { mt_mode = false; }
-  if (cmd == 'M' || btn == 12) { mt_mode = true; }
-  if (cmd == 'O' || btn == 94) { vacuumOnOff(true); }	// Turn on vacuum via BT 'O' or IR num_3
-  if (cmd == 'F' || btn == 74) { vacuumOnOff(false); }	// Turn off vacuum via BT 'F' or IR num_9
+  if (cmd == 'R' || btn == 66) {  // Switch to AI mode via BT 'R' or IR num_1
+    mt_mode = false;
+    btn = 0;    // Prevent recursive call
+    cmd = 'e';
+  }
+  if (cmd == 'M' || btn == 12) {  // Switch to manual mode via BT 'M" or IR num_7
+    mt_mode = true;
+    btn = 0;
+    cmd = 'e';
+  }
+  if (cmd == 'O' || btn == 94) {	// Turn on vacuum via BT 'O' or IR num_3
+    vacuumOnOff(true);
+    btn = 0;
+    cmd = 'e';
+  }
+  if (cmd == 'F' || btn == 74) {	// Turn off vacuum via BT 'F' or IR num_9
+    vacuumOnOff(false);
+    btn = 0;
+    cmd = 'e';
+  }
 
-  if (mt_mode) {
-    if (cmd == 'D' || btn == 90) { right(); }		// Turn right via BT 'D' or IR num_6
-    if (cmd == 'C' || btn == 8) { left(); }		// Turn left via BT 'C' or IR num_3
-    if (cmd == 'S' || btn == 28) { stop(); }		// Set pwmSpeed 0 via BT 'S' or IR num_5
-    if (cmd == 'A' || btn == 24) {// Chnage direction to forward via BT 'A' or IR num_2
-      if (!mov) {
-        forward(1);
-        mov = true;
-      }	// If last direction is reverse then set pwmSpeed 0 and change direction
-      else {
-        forward(0);
-        mov = true;
-      }
-      mov = true;			// Set last direction to forward
-    }
-    if (cmd == 'B' || btn == 82) {// Change direction to rear via BT 'B' or IR num_8
-      if (mov) { reverse(1); }	// If last direction is reverse then set pwmSpeed 0 and change direction
-      else { reverse(0); }
-      mov = false;		// Set last direction to reverse
-    }
+  if (mt_mode) { // 수동 조작 모드이면
+    if (!scanWall()) { hybridRC(); } // 근방에 장애물이 없다면
+    else { stop(); }  // 근방에 장애물이 있다면
   }
   else { scanWay(); } // AI 모드라면
 }
